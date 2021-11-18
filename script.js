a5508145252bc31058fe47f50fcfa17b679b7393
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 16:27:58 +0100

    create function

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-document.getElementById('up').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.top);
-    if(t > 0){
-        t = t - 10;
-        document.getElementById('kenny').style.top = t + 'px';
-    }
-});
-
-document.getElementById('down').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.top);
-    if(t < 500 - 32){
-        t = t + 10;
-        document.getElementById('kenny').style.top = t + 'px';
-    }
-});
-
-document.getElementById('left').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.left);
-    if(t > 50){
-        t = t - 10;
-        document.getElementById('kenny').style.left = t + 'px';
-    }
-    else {
-        document.getElementById('kenny').style.top =
-        alert('Kenny est mort');
-    }
-});
-
-document.getElementById('right').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.left);
-    if(t < 500 - 32){
-        t = t + 10;
-        document.getElementById('kenny').style.left = t + 'px';
-    }
-});
+let death = 0;
+let info = document.createElement('span');
+info.style.marginRight = "20px";
+document.getElementById('buttons').prepend(info);
 
 let redZone = document.createElement('div');
 redZone.style.width = '50px';
@@ -42,6 +11,68 @@ redZone.style.backgroundColor = 'red';
 let frame = document.getElementById('viewport');
 frame.appendChild(redZone);
 
-// if Kenny est mort => alert();
-// display count death
+document.body.addEventListener('keypress', function (event){
+
+    switch (event.key){
+        case "4" :
+            goLeft();
+            break;
+        case "6" :
+            goRight();
+            break;
+        case "8" :
+            goTop();
+            break;
+        case "2" :
+            goDown();
+            break;
+    }
+})
+
+function goLeft () {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    if(t > 50){
+        t = t - 10;
+        document.getElementById('kenny').style.left = t + 'px';
+    }
+    else {
+        death++;
+        alert('Kenny est mort ' + death + " fois");
+        document.getElementById('kenny').style.top = '200px';
+        document.getElementById('kenny').style.left = '200px';
+        info.innerHTML = "nombre de mort : " + death;
+    }
+}
+
+function goRight (){
+    let t = parseInt(document.getElementById('kenny').style.left);
+    if(t < 500 - 32){
+        t = t + 10;
+        document.getElementById('kenny').style.left = t + 'px';
+    }
+}
+
+function goTop (){
+    let t = parseInt(document.getElementById('kenny').style.top);
+    if(t > 0){
+        t = t - 10;
+        document.getElementById('kenny').style.top = t + 'px';
+    }
+}
+
+function goDown (){
+    let t = parseInt(document.getElementById('kenny').style.top);
+    if(t < 500 - 32){
+        t = t + 10;
+        document.getElementById('kenny').style.top = t + 'px';
+    }
+}
+
+document.getElementById('up').addEventListener('click',goTop);
+document.getElementById('down').addEventListener('click',goDown);
+document.getElementById('left').addEventListener('click',goLeft);
+document.getElementById('right').addEventListener('click',goRight);
+
+
+
 
